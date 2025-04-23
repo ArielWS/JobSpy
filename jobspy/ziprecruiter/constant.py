c7454e2eccba91753d8d147643aa2b4cadc52d5c
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-04-24 00:28:14 +0200

    Added additoianl user agents for rotation

--- jobspy/ziprecruiter/constant.py
+++ jobspy/ziprecruiter/constant.py
@@ -1,14 +1,24 @@
+# Updated headers with multiple user-agent options for rotation
 headers = {
     "Host": "api.ziprecruiter.com",
     "accept": "*/*",
     "x-zr-zva-override": "100000000;vid:ZT1huzm_EQlDTVEc",
     "x-pushnotificationid": "0ff4983d38d7fc5b3370297f2bcffcf4b3321c418f5c22dd152a0264707602a0",
     "x-deviceid": "D77B3A92-E589-46A4-8A39-6EF6F1D86006",
-    "user-agent": "Job Search/87.0 (iPhone; CPU iOS 16_6_1 like Mac OS X)",
     "authorization": "Basic YTBlZjMyZDYtN2I0Yy00MWVkLWEyODMtYTI1NDAzMzI0YTcyOg==",
     "accept-language": "en-US,en;q=0.9",
 }
 
+# List of user agents for rotation
+user_agents = [
+    "Job Search/87.0 (iPhone; CPU iOS 16_6_1 like Mac OS X)",
+    "Job Search/91.0 (iPhone; CPU iOS 16_7 like Mac OS X)",
+    "Job Search/85.0 (iPhone; CPU iOS 15_5 like Mac OS X)",
+    "Job Search/88.0 (iPhone; CPU iOS 16_6_1 like Mac OS X)",
+    "Job Search/90.0 (iPhone; CPU iOS 15_3 like Mac OS X)",
+]
+
+# Get cookie data remains unchanged, but we might want to ensure it gets updated when the user-agent changes
 get_cookie_data = [
     ("event_type", "session"),
     ("logged_in", "false"),
@@ -27,3 +37,4 @@ get_cookie_data = [
     ("property", "device_model:iPhone 14 Pro"),
     ("property", "brand:Apple"),
 ]
+
